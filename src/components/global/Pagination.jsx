cfb9e995c977fc180bb295fbccd8ffae920e0c1a
zackcole <183650880+zackcole@users.noreply.github.com>
2025-07-02 06:15:03 -0700

    update:filtets

--- src/components/global/Pagination.jsx
+++ src/components/global/Pagination.jsx
@@ -5,13 +5,15 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   return (
     <div className="flex justify-center mt-4 gap-2">
       <button
-        className="px-3 py-1 bg-gray-200 rounded disabled:opacity-50"
+        className={`px-3 py-1 bg-blue-500 text-white rounded disabled:opacity-80 disabled:cursor-not-allowed`}
         disabled={currentPage === 1}
         onClick={() => onPageChange(currentPage - 1)}
       >
         Prev
       </button>
+      <span className="px-3 py-1">{`Page ${currentPage} of ${totalPages}`}</span>
       <button
+        className="bg-blue-500 px-4 rounded-md text-white disabled:opacity-80 disabled:cursor-not-allowed"
         disabled={currentPage >= totalPages}
         onClick={() => onPageChange(currentPage + 1)}
       >
