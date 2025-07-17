b8d735f922ce8d148613003a47f368995b62294a
mathewgarfield <189109425+mathewgarfield@users.noreply.github.com>
2025-07-17 05:30:33 -0700

    Add:IP Dev

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 
 // export const baseUrl = "https://managementapi.zuimp.online";
-export const baseUrl = "https://devmanagementapi.zuimp.online/";
+export const baseUrl = "https://devmanagementapi.zuimp.online";
 // export const baseUrl = "http://192.168.9.42:3050";
 // export const baseUrl = "https://155e-45-199-187-86.ngrok-free.app";
 
